d082577ebfe2dddb5435e780c12d4c2fa370c1ed
Karnankita04 <91003709+Karnankita04@users.noreply.github.com>
2024-12-19 15:47:21 +0530

    executed echo

--- nutshell.js
+++ nutshell.js
@@ -1,1 +1,16 @@
-console.log("hii");
+const executeEcho = function (msg) {
+  return msg.join(" ");
+};
+
+const executeCommand = function ([cmd, ...msg]) {
+  switch (cmd) {
+    case "echo": return executeEcho(msg);
+  }
+};
+
+while (true) {
+  const command = prompt("ankitakarn@Ankitas-MacBook-Pro shell %");
+  const output = executeCommand(command.split(" "));
+  
+  console.log(output);
+}

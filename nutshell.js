14db4be96a0819ffd2bd572e9841d14df9d2bf6a
Karnankita04 <91003709+Karnankita04@users.noreply.github.com>
2024-12-19 18:02:41 +0530

    added touch

--- nutshell.js
+++ nutshell.js
@@ -1,12 +1,22 @@
 let currDir = "~";
-const path = "ankitakarn@Ankitas-MacBook-Pro ";
+const path = "ankitakarn@Nishus-MacBook-Pro ";
+const directory = [currDir];
+const files = [];
 
 const executeEcho = function (msg) {
   return msg.join(" ");
 };
 
 const executeCd = function (msg) {
-  currDir = msg.join(" ");
+  directory.push(msg.join(" "));
+};
+
+const executeLs = function () {
+  return files.join("         ");
+};
+
+const executeTouch = function (msg) {
+  files.push(msg);
 };
 
 const executeCommand = function ([cmd, ...msg]) {
@@ -14,15 +24,18 @@ const executeCommand = function ([cmd, ...msg]) {
     case "echo": return executeEcho(msg);
 
     case "cd": return executeCd(msg);
+
+    case "ls": return executeLs();
+
+    case "touch": return executeTouch(msg);
   }
 };
 
 while (true) {
-  const command = prompt(path + currDir + " %");
+  const command = prompt(path + directory.at(-1) + " %");
   const output = executeCommand(command.split(" "));
 
   if (output !== undefined) {
     console.log(output);
-
   }
 }
